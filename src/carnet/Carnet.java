8d32ab496a9ff2e95a9b24aec87da42b720673e2
Medrano65 <america.medrano65@gmail.com>
2025-05-15 15:40:32 -0600

    My first commit

---
+++ src/carnet/Carnet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carnet;
+
+/**
+ *
+ * @author Harim
+ */
+public class Carnet {
+
+   public static void main(String[] args) {
+        String carnetId = "MM00130";
+        System.out.println("El numero de carnet del estudiante es: " + carnetId);
+    }
+}
